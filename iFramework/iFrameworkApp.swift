bc11a483b79d5b9352d5873c751e22c59d6d52f7
sanayuj <sanayuj2255@gmail.com>
2024-02-18 18:38:36 +0530

    first commit

---
+++ iFramework/iFrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  iFrameworkApp.swift
+//  iFramework
+//
+//  Created by Sanay UJ on 18/02/24.
+//
+
+import SwiftUI
+
+@main
+struct iFrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
